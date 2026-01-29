bb2045bfc3ce452eac3c8d5f1b295e94b6c94777
Adelino Sousa Manuel <adilsonsousaas82@gmail.com>
2026-01-29 20:58:42 +0100

    feat: update

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -8,7 +8,9 @@ public class Board {
 	private Piece[][] pieces;
 	//	12- O construtor vai apenas receber a quantidade de linhas e colunas
 	public Board(int rows, int columns) {
+		//	64- Colocando condições para lançar uma exeção
 		if(rows < 1 || columns < 1) {
+			//	65- Exceção para mostrar que há erro ao criar tabuleiro dizendo que é necessário uma linha ou uma coluna
 			throw new BoardException("Error creating board: there must be at least 1 row and 1 column");
 		}
 		this.rows = rows;
@@ -26,7 +28,9 @@ public class Board {
 	}
 	//	15- Metodo para retornar a peça dada uma linha e uma coluna	
 	public Piece piece(int row, int column) {
+		//	66- Testando: se a posição na linha e coluna indicada não existe então vai lançar uma exceção
 		if(!positionExists(row, column)) {
+			//	67- Lançando uma nova BoardException contendo a informação de que a posição não está no tabuleiro
 			throw new BoardException("Position not on the board");
 		}
 		//	16- Vai retornar a matriz piece na linha row e na coluna column
@@ -39,8 +43,9 @@ public class Board {
 	}
 	//	42- Criamos o metodo placePiece que é o responsável por colocar a peça na posição indicada no tabuleiro
 	public void placePiece(Piece piece, Position position) {
-		//	
+		//	68- Testando novamente mas dessa vez de acordo a posição
 		if(thereIsAPiece(position)) {
+			//	69- Se há na verdade uma posição então ela vai imprimir
 			throw new BoardException("There is already a piece on position " + position);
 		}
 		//	43- Vai pegar na matriz de peças na posição dada e colocar os valores que vieram no metodo como argumento
@@ -62,19 +67,24 @@ public class Board {
 		pieces[position.getRow()][position.getColumn()] = null;
 		return aux;
 	}
-	//	58- positionExists
+	//	59- positionExists pela linha e pela coluna
 	private boolean positionExists(int row, int column) {
+		//	60- Verificando se a posição está correta de acordo a linha e a coluna
 		return row >= 0 && row < rows && column >= 0 && column < columns;
 	}
-	
+	//	58- positionExists pela posição das peças
 	public boolean positionExists(Position position) {
+		//	61- reaproveitamos o método de cima para testar se a posição existe baseando-se na posição apartir da linha e da coluna
 		return positionExists(position.getRow(), position.getColumn());		
 	}
-	
+	//	62- Metodo que irá receber uma posição, vamos testar se tem uma peça nessa posição
 	public boolean thereIsAPiece(Position position) {
+		//	70- Faz o teste se realmente existe uma peça na posição
 		if(!positionExists(position)) {
+			//	71- Testamos se há peça no tabuleiro de acordo a posição
 			throw new BoardException("Position not on the board");
 		}
+		//	63- Se tiver uma peça diferente de nula, vai retornar a posição dessa peça
 		return piece(position) != null;
 		
 	}
